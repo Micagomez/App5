bc959da48976a5bae8a3e9d0899c726655317276
Angel Gomez <angelezequielgomez15@gmail.com>
2024-11-12 09:49:59 -0300

    sidebar doctor added, dashboard doctor added

---
+++ www/app/views/include/sidebar_doctor.php
@@ -0,0 +1,77 @@
+<div class="sidebar app-aside" id="sidebar">
+	<div class="sidebar-container perfect-scrollbar">
+		<nav>
+			<ul class="main-navigation-menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-home"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Inicio </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Historiales Clínicos </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="lastMedicalHistory.php">
+								<span class="title"> Últimos Historiales Agregados </span>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Pacientes </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="patientsList.php">
+								<span class="title"> Lista de pacientes </span>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-folder"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title">Config globales</span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="editProfile.php">
+								<span class="title"> Editar perfil </span>
+							</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</div>
